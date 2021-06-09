ca13b0d44b7fce75a34d69c2013f52f9176ac9bb
Claudio Rojas Rodriguez <claudiorojasrodriguez@Claudios-MBP.bbrouter>
2021-06-09 03:53:35 -0400

    Stable code

---
+++ src/store/index.ts
@@ -0,0 +1,18 @@
+import { createStore, combineReducers, compose } from 'redux'
+import userReducer from './users'
+
+declare global {
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION__?: typeof compose;
+    }
+}
+
+const rootReducer = combineReducers({
+    users: userReducer,
+}) 
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+export default store
